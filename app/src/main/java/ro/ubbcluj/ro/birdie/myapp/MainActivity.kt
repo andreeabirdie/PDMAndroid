fb4c63fb05192f30b9b9009ce234b2ac8748d08e
Vrabie Andreea <andreea.birdie@yahoo.com>
2020-11-28 18:42:37 +0200

    show when app is online/offline

--- app/src/main/java/ro/ubbcluj/ro/birdie/myapp/MainActivity.kt
+++ app/src/main/java/ro/ubbcluj/ro/birdie/myapp/MainActivity.kt
@@ -1,20 +1,77 @@
 package ro.ubbcluj.ro.birdie.myapp
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
+import android.net.*
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import android.view.Menu
+import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.coroutines.InternalCoroutinesApi
 import ro.ubbcluj.ro.birdie.myapp.core.TAG
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var connectivityManager: ConnectivityManager
+    private lateinit var connectivityLiveData: ConnectivityLiveData
 
+    @RequiresApi(Build.VERSION_CODES.M)
     @InternalCoroutinesApi
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(findViewById(R.id.toolbar))
         Log.i(TAG, "onCreate")
+        connectivityManager = getSystemService(android.net.ConnectivityManager::class.java)
+        connectivityLiveData = ConnectivityLiveData(connectivityManager)
+    }
+
+    @RequiresApi(Build.VERSION_CODES.N)
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "isOnline ${isOnline()}")
+        connectivityManager.registerDefaultNetworkCallback(networkCallback)
+    }
+
+    @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
+    override fun onStop() {
+        super.onStop()
+        connectivityManager.unregisterNetworkCallback(networkCallback)
+    }
+
+    private fun isOnline(): Boolean {
+        val connMgr = getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val networkInfo: NetworkInfo? = connMgr.activeNetworkInfo
+        return networkInfo?.isConnected == true
+    }
+
+    private val networkCallback = @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
+    object : ConnectivityManager.NetworkCallback() {
+        override fun onAvailable(network: Network) {
+            runOnUiThread {
+                networkTxt.text = getString(R.string.active_network)
+                networkIc.setImageResource(R.drawable.ic_active_network)
+            }on
+        }
+
+        override fun onLost(network: Network) {
+            runOnUiThread {
+                networkTxt.text = getString(R.string.inactive_network)
+                networkIc.setImageResource(R.drawable.ic_inactive_network)
+            }
+        }
+
+        override fun onCapabilitiesChanged(
+            network: Network,
+            networkCapabilities: NetworkCapabilities
+        ) {
+            Log.d(TAG, "The default network changed capabilities: $networkCapabilities")
+        }
+
+        override fun onLinkPropertiesChanged(network: Network, linkProperties: LinkProperties) {
+            Log.d(TAG, "The default network changed link properties: $linkProperties")
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
